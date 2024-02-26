434ac41ff28fc3a110f782bcbe85a8c6b304011b
Alex Zhytar <olexandr.jhytar@artilab.pro>
2024-02-26 12:10:16 +0200

    banner slider

--- src/components/BlockWP/blocks/BannerMain/bannerMainWords.jsx
+++ src/components/BlockWP/blocks/BannerMain/bannerMainWords.jsx
@@ -10,19 +10,19 @@ export default function BannerMainWords( {getWord} ) {
     className: 'words-slider',
     variableWidth: true,
     autoplay: true,
-    speed: 1500,
+    speed: 10000,
     draggable: false,
-    cssEase: "ease",
-    autoplaySpeed: 4000,
-    // beforeChange: ( nextIndex ) => {
-    //   let index = document.querySelector('.words-slider .slick-slide').dataset.index;
-    //   if (nextIndex === (Math.abs(index) - 1)) {
-    //     document.querySelector(`.words-slider .slick-slide[data-index="${Math.abs(index)}"]`).dataset.slide = 'active';
-    //     setTimeout(() => {
-    //       document.querySelector(`.words-slider .slick-slide[data-index="${Math.abs(index)}"]`).removeAttribute('data-slide');
-    //     }, 4000);
-    //   }
-    // },
+    cssEase: "linear",
+    autoplaySpeed: 0,
+    beforeChange: ( nextIndex ) => {
+      let index = document.querySelector('.words-slider .slick-slide').dataset.index;
+      if (nextIndex === (Math.abs(index) - 1)) {
+        document.querySelector(`.words-slider .slick-slide[data-index="${Math.abs(index)}"]`).dataset.slide = 'active';
+        setTimeout(() => {
+          document.querySelector(`.words-slider .slick-slide[data-index="${Math.abs(index)}"]`).removeAttribute('data-slide');
+        }, 10000);
+      }
+    },
     arrows: false,
     responsive: [
       {
